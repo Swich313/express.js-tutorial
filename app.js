53bf09ec4e59bdef3b4f4c4d27d0fa809b88cc06
Swich <andruha313@gmail.com>
2022-02-10 11:04:38 +0200

    express server with 3 pages using pug, ejs, handlebars templates

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
+
+// app.set('view engine', 'pug');       for pug template
+
+// app.engine('handlebars', expressHbs({        for handleBar template
+//     layoutsDir: 'views/layout/',
+//     defaultLayout: 'main-layout',
+//     extname: 'handlebars'
+// }))
+//app.set('view engine', 'handlebars');
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const PORT = 3000;
@@ -11,11 +24,11 @@ const PORT = 3000;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(PORT, () => {
