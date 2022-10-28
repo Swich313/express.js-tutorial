bb5b5737b921d34a29e739bbacca1e1a9c74d085
Swich <andruha313@gmail.com>
2022-10-28 16:22:16 +0300

    add login form and set cookie

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 const PORT = 3000;
 
@@ -41,6 +42,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.notFoundPage);
 
